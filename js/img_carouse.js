575c787a58f53536124dad838737167e2c3caa3f
Kuronitz <43643741+Kuronitz@users.noreply.github.com>
2024-10-20 18:12:29 +0800

    first commit

---
+++ js/img_carouse.js
@@ -0,0 +1,11 @@
+setInterval(function () {
+    document.getElementById("indicator1").click();
+}, 3000);
+
+setInterval(function () {
+    document.getElementById("indicator2").click();
+}, 6000);
+
+setInterval(function () {
+    document.getElementById("indicator3").click();
+}, 9000);
